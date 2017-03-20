ee46254989d23307260e74a0cad73a9d0a83b2b5
Billiam Stout <wrstout55@gmail.com>
2017-03-20 00:59:52 -0600

    adds start of chap 4

---
+++ ch4.py
@@ -0,0 +1,40 @@
+# def f(x):
+#     return x * 2
+
+# result = f(2)
+# print(result)
+
+#---------
+# def f(x,y,z):
+#     return x + y + z
+
+# result = f(1, 2, 3)
+# print(result)
+#---------
+# age = input("Enter your age:")
+# int_age = int(age)
+# if int_age < 21:
+#     print("you are young!")
+# else:
+#     print("Wow, you are old!")
+#---------
+# def even_odd():
+#     n = input("type a number:")
+#     n = int(n)
+#     if n % 2 == 0:
+#         print ("n is even.")
+#     else:
+#         print ("n is odd.")
+
+# even_odd()
+# even_odd()
+# even_odd()
+
+def f(x=2) :
+    return x**x
+
+print (f())
+print (f(4))
+
+
+
